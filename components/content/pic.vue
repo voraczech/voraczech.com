33bce19fc47b6594ffea1115b8cf3ffdfb2abf86
Jakub <jakub.voracek@gmail.com>
2024-08-18 19:21:48 +0400

    add pic for better loading

---
+++ components/content/pic.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  src: String,
+  alt: String,
+  loading: {
+    type: String,
+    default: "lazy",
+  },
+})
+</script>
+
+<template>
+  <NuxtImg
+    :src="src"
+    :alt="alt"
+    :loading="loading"
+    :modifiers="{ rotate: null }"
+    class="rounded-md my-6 mx-auto"
+    width="100%"
+    sizes="320px sm:450px md:512px lg:740px"
+  />
+</template>
